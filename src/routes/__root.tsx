1bff88c4825ff9ffa717ee9b04be4ba74c4a5676
Daniel Chicchon <danielchicchon@gmail.com>
2025-02-13 14:32:08 -0800

    added deployment type check for showing tanstack debugger

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,15 @@
 import { createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+// check the build type
 import App from '../App';
+
+const dev = import.meta.env.DEV;
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <App />
-      <TanStackRouterDevtools />
+      {dev && <TanStackRouterDevtools />}
     </>
   ),
 });
